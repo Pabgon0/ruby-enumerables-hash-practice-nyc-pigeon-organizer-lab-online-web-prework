92abbed7f7c224a566e3870b577b6fb312de7a28
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-10 17:49:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,20 +12,35 @@ pigeon_list = {}
     end
   end
 hash_layout = pigeon_list.keys
-  data[:color].each do |color, color_name|
-    color_name.each do |name|
-      hash_layout.each do |bird_name|
-        if bird_name == name
-          pigeon_list[name][:color] << "#{color}"
-        end
+data[:color].each do |color, color_name|
+  color_name.each do |name|
+    hash_layout.each do |bird_name|
+      if bird_name == name
+        pigeon_list[name][:color] << "#{color}"
       end
     end
   end
+end
+
+data[:gender].each do |gender, gender_name|
+  gender_name.each do |name|
+    hash_layout.each do |bird_name|
+      if bird_name == name
+        pigeon_list[name][:gender] << "#{gender}"
+      end
+    end
+  end
+end
   
-  
-  
-  
-  
+data[:lives].each do |area, area_name|
+  area_name.each do |name|
+    hash_layout.each do |bird_name|
+      if bird_name == name
+        pigeon_list[name][:lives] << "#{area}"
+      end
+    end
+  end
+end 
   
   pigeon_list
 end
