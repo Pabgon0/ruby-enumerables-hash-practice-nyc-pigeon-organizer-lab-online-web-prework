e41a93c765dcb116e4c1ed30fa91e3598e654c05
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-10 17:36:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,19 +4,17 @@ pigeon_list = {}
     info.each do |other_key, bird_names|
       bird_names.each do |name|
         pigeon_list[name] = {
-          :color => []
-          :gender => []
+          :color => [],
+          :gender => [],
           :lives => []
         }
       end
     end
   end
 hash_layout = pigeon_list
-  data.each do |key, info|
-    info.each do |other_key, bird_names|
-      bird_names.each do |name|
-        
-      end
+  data[:color].each do |color, color_name|
+    color_name.each do |name|
+      
     end
   end
   
