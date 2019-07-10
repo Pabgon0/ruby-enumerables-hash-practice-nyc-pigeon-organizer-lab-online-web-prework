9173b05037c296e63c44d142215277ea8ebb5a7a
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-10 16:56:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key, info|
+    info.each do |other_key, bird_names|
+      bird_names.each do |name|
+        if pigeon_list.value?(name) == false
+          pigeon_list[]
+        end
+      end
+    end
+  end
+  pigeon_list
 end
